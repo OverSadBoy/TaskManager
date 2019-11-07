8745c8a6b55b5a3790eb9cacc02b3e9a35d47810
michael <preload737773@gmail.com>
2019-11-07 17:16:03 +0400

    serialization

--- src/net/lab/JournalManagement.java
+++ src/net/lab/JournalManagement.java
@@ -1,3 +1,17 @@
 package net.lab;
+
+import java.io.*;
+
 public class JournalManagement {
+    public static void serializeTasks(TaskJournal object, FileOutputStream fStream) throws IOException {
+        ObjectOutputStream oos = new ObjectOutputStream(fStream);
+        oos.writeObject(object);
+    }
+
+    public static TaskJournal deserializeTasks(FileInputStream fStream) throws IOException, ClassNotFoundException {
+        TaskJournal result = new TaskJournal();
+        ObjectInputStream ois = new ObjectInputStream(fStream);
+        result = (TaskJournal) ois.readObject();
+        return result;
+    }
 }
