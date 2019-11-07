22a45080a899fb5e646089c641cca795934975ee
michael <preload737773@gmail.com>
2019-11-07 17:23:00 +0400

    beginning

---
+++ src/mainForm.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class mainForm {
+    private JTable table1;
+    private JPanel panel1;
+    private JButton addTaskButton;
+    private JButton deleteTaskButton;
+}
