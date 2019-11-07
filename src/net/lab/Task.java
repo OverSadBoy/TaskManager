be382c13e47527c197a772a4d159d1bcd60bd805
michael <preload737773@gmail.com>
2019-11-07 16:38:41 +0400

    beginning

---
+++ src/net/lab/Task.java
@@ -0,0 +1,48 @@
+import java.io.Serializable;
+import java.util.Date;
+
+public class Task implements Serializable {
+    private String name;
+    private String description;
+    private Date notificationDate;
+    private String contacts;
+
+    public Task(String name, String description, Date notificationDate, String contacts) {
+        this.name = name;
+        this.description = description;
+        this.notificationDate = notificationDate;
+        this.contacts = contacts;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getNotificationDate() {
+        return notificationDate;
+    }
+
+    public void setNotificationDate(Date notificationDate) {
+        this.notificationDate = notificationDate;
+    }
+
+    public String getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(String contacts) {
+        this.contacts = contacts;
+    }
+}
---
+++ src/net/lab/TaskJournal.java
@@ -0,0 +1,21 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class TaskJournal implements Serializable {
+    private ArrayList<Task> tasks = new ArrayList<Task>();
+
+    public TaskJournal(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public TaskJournal() {
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void deleteTask(int taskNumber) {
+        tasks.remove(taskNumber);
+    }
+}
