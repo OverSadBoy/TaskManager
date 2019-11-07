96499d71c3b7de0c4933bb652dc0d3fcf4b8d51b
michael <preload737773@gmail.com>
2019-11-07 17:36:31 +0400

    beginning

--- src/mainForm.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class mainForm {
-    private JTable table1;
-    private JPanel panel1;
-    private JButton addTaskButton;
-    private JButton deleteTaskButton;
-}
--- src/net/lab/Main.java
+++ src/net/lab/Main.java
@@ -3,6 +3,6 @@ package net.lab;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+         MainForm form = new MainForm("Task Manager");
     }
 }
