45e3bf1db8a38cd998fa0aa32c3fd063678e68fb
michael <preload737773@gmail.com>
2019-11-21 18:37:49 +0400

    added xml read/write support

--- src/net/lab/model/Model.java
+++ src/net/lab/model/Model.java
@@ -2,40 +2,73 @@ package net.lab.model;
 
 import net.lab.controller.Controller;
 
+import javax.xml.stream.*;
 import java.io.*;
+import java.text.SimpleDateFormat;
 import java.util.Vector;
 
 public class Model implements ModelContract {
 
-    private static final String fileName = "data.bin";
-    private File file;
+    private static final String fileName = "Data.xml";
+   // private File file;
     private static FileInputStream fileInputStream;
     private static FileOutputStream fileOutputStream;
-
-    private static Vector<Task> listTask;
+    private static Vector<Task> taskList;
 
     public Model() throws IOException {
-        file = new File(fileName);
-        if(!file.isFile()) {
-            file.createNewFile();
-        }
+//        file = new File(fileName);
+//        if(!file.isFile()) {
+//            file.createNewFile();
+//        }
 
-        try {
-            fileInputStream = new FileInputStream(file);
-            fileOutputStream = new FileOutputStream(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            fileInputStream = new FileInputStream(file);
+//            fileOutputStream = new FileOutputStream(file);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+        var tasks = new Vector<Task>();
+        tasks.add(0, new Task("Rabota", "OnFriday", new SimpleDateFormat("27.10.2019"), "89297928290"));
+        tasks.add(0, new Task("Rabota2", "OnFriday", new SimpleDateFormat("28.10.2019"), "89297928290"));
+        tasks.add(0, new Task("Rabota3", "OnFriday", new SimpleDateFormat("29.10.2019"), "89297928290"));
+        tasks.add(0, new Task("Rabota4", "OnFriday", new SimpleDateFormat("30.10.2019"), "89297928290"));
+        addTask(tasks);
     }
 
     @Override
     public void addTask(Vector<Task> tasks) {
         try {
-            ObjectOutputStream oos = new ObjectOutputStream(fileOutputStream);
-            new FileWriter(file).write("");
-            oos.writeObject(tasks);
-            oos.close();
-        } catch (IOException e) {
+            XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
+            XMLStreamWriter writer = outputFactory.createXMLStreamWriter(new FileWriter(fileName));
+            writer.writeStartDocument("1.0");
+            writer.writeStartElement("Tasks");
+            for (var task :
+                    tasks) {
+                writer.writeStartElement("Task");
+                //Name
+                writer.writeStartElement("Name");
+                writer.writeCharacters(task.getName());
+                writer.writeEndElement();
+                //Description
+                writer.writeStartElement("Description");
+                writer.writeCharacters(task.getDescription());
+                writer.writeEndElement();
+                //Notification Date
+                writer.writeStartElement("NotificationDate");
+                writer.writeCharacters(task.getNotificationDate().toPattern());
+                writer.writeEndElement();
+                //Contacts
+                writer.writeStartElement("Contacts");
+                writer.writeCharacters(task.getContacts());
+                writer.writeEndElement();
+
+                writer.writeEndElement();
+            }
+            writer.writeEndElement();
+            writer.writeEndDocument();
+            //writer.flush();
+            writer.close();
+        } catch (XMLStreamException | IOException e) {
             e.printStackTrace();
         }
     }
@@ -43,11 +76,20 @@ public class Model implements ModelContract {
     @Override
     public Vector<Task> getTasks() {
         Vector<Task> tasks = new Vector<>();
+        XMLInputFactory inputFactory = XMLInputFactory.newInstance();
         try {
-            ObjectInputStream ois = new ObjectInputStream(fileInputStream);
-            tasks = (Vector<Task>) ois.readObject();
-            ois.close();
-        } catch (IOException | ClassNotFoundException e) {
+            XMLStreamReader reader = inputFactory.createXMLStreamReader(fileName, fileInputStream);
+            while (reader.hasNext()) {
+                reader.next();
+                if (reader.isStartElement()) {
+                    System.out.println(reader.getLocalName());
+                } else if (reader.isEndElement()) {
+                    System.out.println("/" + reader.getLocalName());
+                } else if (reader.hasText() && reader.getText().trim().length() > 0) {
+                    System.out.println("   " + reader.getText());
+                }
+            }
+        } catch (XMLStreamException e) {
             e.printStackTrace();
         }
         return tasks;
--- src/net/lab/model/Task.java
+++ src/net/lab/model/Task.java
@@ -1,59 +1,31 @@
 package net.lab.model;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Task implements Serializable {
-    private int id;
-    private String name;
-    private String description;
-    private String notificationDate;
-    private String contacts;
+    private final String name;
+    private final String description;
+    private final SimpleDateFormat notificationDate;
+    private final String contacts;
 
-    public Task(String name, String description, String notificationDate, String contacts) {
+    public Task(String name, String description, SimpleDateFormat notificationDate, String contacts) {
         this.name = name;
         this.description = description;
         this.notificationDate = notificationDate;
         this.contacts = contacts;
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getDescription() {
         return description;
     }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getNotificationDate() {
+    public SimpleDateFormat getNotificationDate() {
         return notificationDate;
     }
-
-    public void setNotificationDate(String notificationDate) {
-        this.notificationDate = notificationDate;
-    }
-
     public String getContacts() {
         return contacts;
-    }
-
-    public void setContacts(String contacts) {
-        this.contacts = contacts;
     }
 }
